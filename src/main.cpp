635df05550997d022e5e85d9aea183439cc972d0
Matthew Barulic <mattb@wheego.net>
2018-07-13 17:10:52 -0400

    Changes QApplication::exec() call to actually look like the static member access that it is.

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,5 @@ int main(int argc, char** argv)
   QApplication app(argc, argv);
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-  return app.exec();
+  return QApplication::exec();
 }
